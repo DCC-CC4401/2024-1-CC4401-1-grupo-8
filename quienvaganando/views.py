60da82e0ef772dce61106793b00c4a4a73c8fc75
Claudio Gaete <claudio.gaete@ug.uchile.cl>
2024-05-03 17:22:27 -0400

    FEAT Iniciar overview de torneos

--- quienvaganando/views.py
+++ quienvaganando/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from quienvaganando.models import *
 
-# Create your views here.
+
+def overview_torneo(request, uuid_torneo):
+    
+    torneo = Torneo.objects.get(uuid=uuid_torneo)
+    
+    # 
+    
+    if request.method == "GET":
+        return render(request, "quienvaganando/overview_torneo.html", {
+            "nombre": torneo.nombre,
+            "eventos": [],
+            "header_tabla": ["Pos.", "Equipo", "1°", "2°", "3°", "Ptje."],
+            "datos_tabla": [
+                {1, "DCC", 2, 1, 3, 4000},
+                {2, "FIAS", 1, 0, 0, 200}
+            ]
+        })
