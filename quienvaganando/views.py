2f63f23e91a2d4fba38d9c065ba58dbbf73bfdcd
Matías Miranda <matias.miranda.c@ug.uchile.cl>
2024-05-09 16:50:48 -0400

    user_register view

--- quienvaganando/views.py
+++ quienvaganando/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from quienvaganando import User
 
 # Create your views here.
+from django.http import HttpResponseRedirect
+
+def register_user(request):
+    if request.method == 'GET': #Si estamos cargando la página
+     return render(request, "todoapp/register_user.html") #Mostrar el template
+
+    elif request.method == 'POST': #Si estamos recibiendo el form de registro
+     #Tomar los elementos del formulario que vienen en request.POST
+     nombre = request.POST['nombre']
+     contraseña = request.POST['contraseña']
+    
+     #Crear el nuevo usuario
+     user = User.objects.create_user(username=nombre, password=contraseña)
+
+     #Redireccionar la página /tareas
+     return HttpResponseRedirect('/') # CAMBIAR RUTA
