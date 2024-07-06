54486f49bddd7aea8af3e4343c795567d1379318
Claudio Gaete <claudio.gaete@ug.uchile.cl>
2024-07-06 12:22:17 -0400

    Agregar URL de edición de participantes

--- quienvaganando/urls.py
+++ quienvaganando/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('torneos/', views.lista_torneos, name='torneos'),
     path('crear_torneo/', views.nuevo_torneo, name='creacion_torneo'),
     path('torneos/<str:uuid_torneo>/', views.overview_torneo, name='overview_torneo'),
+    path('torneos/<str:uuid_torneo>/editar_participantes', views.editar_participantes, name='editar_participantes'),
 ]
